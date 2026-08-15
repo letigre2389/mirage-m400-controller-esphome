d02d39261738394775e509d6a5985182d26ddc9b
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-15 02:00:18 -0600

    Update __init__.py

--- components/mirage_m400/__init__.py
+++ components/mirage_m400/__init__.py
@@ -12,15 +12,16 @@ MirageM400Component = mirage_m400_ns.class_("MirageM400Component", cg.Component,
 CONF_UART_ID = "uart_id"
 CONF_MIRAGE_M400_ID = "mirage_m400_id"
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(MirageM400Component),
-        cv.GenerateID(CONF_UART_ID): cv.use_id(uart.UART),
-    }
-).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = (
+    cv.COMPONENT_SCHEMA.extend(uart.UART_DEVICE_SCHEMA).extend(
+        {
+            cv.GenerateID(): cv.declare_id(MirageM400Component),
+        }
+    )
+)
 
 async def to_code(config):
-    uart_component = await cg.get_variable(config[CONF_UART_ID])
+    uart_component = await cg.get_variable(config[uart.CONF_UART_ID])
     var = cg.new_variable(config[CONF_ID], MirageM400Component)
     cg.add(var.set_uart_parent(uart_component))
     cg.add(cg.register_component(var, config))
