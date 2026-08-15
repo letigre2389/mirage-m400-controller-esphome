2c30442c9984b59ada74363888b4bd3e0e41a647
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-15 14:09:17 -0600

    Update number.py

--- components/mirage_m400/number.py
+++ components/mirage_m400/number.py
@@ -3,6 +3,35 @@ import esphome.config_validation as cv
 from esphome.components import number
 from esphome.const import CONF_ID
 
+from . import mirage_m400_ns, MirageM400Component, MirageM400Number, CONF_MIRAGE_M400_ID
+
+CONF_ZONE = "zone"
+
+NUMBER_SCHEMA = number.NUMBER_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(MirageM400Number),
+        cv.Required(CONF_ZONE): cv.int_range(min=1, max=16),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config, parent=None):
+    var = cg.new_variable(config[CONF_ID], MirageM400Number)
+    await cg.register_component(var, config)
+    
+    if parent is None:
+        # Fallback for standalone use
+        parent = await cg.get_variable(config.get(CONF_MIRAGE_M400_ID))
+    
+    cg.add(parent.register_number(var))
+    cg.add(var.set_parent(parent))
+    cg.add(var.set_zone(config[CONF_ZONE]))
+    
+    await number.register_number(var, config)
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import number
+from esphome.const import CONF_ID
+
 from . import mirage_m400_ns, MirageM400Component, CONF_MIRAGE_M400_ID
 
 MirageM400Number = mirage_m400_ns.class_("MirageM400Number", cg.Component)
