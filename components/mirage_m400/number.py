4cac345c2bbd60b5eaf25b707c198a0a59c3eb6d
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-15 02:09:05 -0600

    Update number.py

--- components/mirage_m400/number.py
+++ components/mirage_m400/number.py
@@ -12,19 +12,15 @@ CONF_ZONE = "zone"
 mirage_m400_ns = cg.esphome_ns.namespace("mirage_m400")
 MirageM400Number = mirage_m400_ns.class_("MirageM400Number", number.Number)
 
-CONFIG_SCHEMA = cv.Schema(
+CONFIG_SCHEMA = number.number_schema(MirageM400Number).extend(
     {
-        cv.GenerateID(): cv.declare_id(MirageM400Number),
         cv.GenerateID(CONF_MIRAGE_M400_ID): cv.use_id(cg.Component),
         cv.Required(CONF_ZONE): cv.int_range(min=1, max=16),
-        cv.Optional("min_value", default=0): cv.float_,
-        cv.Optional("max_value", default=100): cv.float_,
-        cv.Optional("step", default=1): cv.float_,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_variable(config[CONF_ID], MirageM400Number)
+    var = await number.new_number(config, min_value=0, max_value=100, step=1)
     mirage = await cg.get_variable(config[CONF_MIRAGE_M400_ID])
     cg.add(var.set_parent(mirage))
     cg.add(mirage.register_number(var, config[CONF_ZONE]))
