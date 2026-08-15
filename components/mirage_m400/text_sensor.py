dac80c435c49cd5a6c711128b199d23125225c1f
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-15 14:08:44 -0600

    Update text_sensor.py

--- components/mirage_m400/text_sensor.py
+++ components/mirage_m400/text_sensor.py
@@ -3,22 +3,22 @@ import esphome.config_validation as cv
 from esphome.components import text_sensor
 from esphome.const import CONF_ID
 
-from . import mirage_m400_ns, MirageM400Component, CONF_MIRAGE_M400_ID
-
-MirageM400TextSensor = mirage_m400_ns.class_("MirageM400TextSensor", cg.Component)
+from . import mirage_m400_ns, MirageM400Component, MirageM400TextSensor, CONF_MIRAGE_M400_ID
 
 TEXT_SENSOR_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(MirageM400TextSensor),
-        cv.GenerateID(CONF_MIRAGE_M400_ID): cv.use_id(MirageM400Component),
     }
 )
 
-async def to_code(config):
+async def to_code(config, parent=None):
     var = cg.new_variable(config[CONF_ID], MirageM400TextSensor)
     await cg.register_component(var, config)
     
-    parent = await cg.get_variable(config[CONF_MIRAGE_M400_ID])
+    if parent is None:
+        # Fallback for standalone use
+        parent = await cg.get_variable(config.get(CONF_MIRAGE_M400_ID))
+    
     cg.add(parent.register_text_sensor(var))
     cg.add(var.set_parent(parent))
     
