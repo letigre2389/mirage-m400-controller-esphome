d89f924085b5ca2d25408041afa0cf4aca730ca0
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-15 01:33:25 -0600

    Update __init__.py

--- components/mirage_m400/__init__.py
+++ components/mirage_m400/__init__.py
@@ -3,23 +3,24 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
 
-CODEOWNERS = ["@yourusername"]
-DEPENDENCIES = ["uart"]
+CODEOWNERS = ["@your_github_username"]
 
 mirage_m400_ns = cg.esphome_ns.namespace("mirage_m400")
 MirageM400Component = mirage_m400_ns.class_("MirageM400Component", cg.Component, uart.UARTDevice)
 
-CONF_MIRAGE_M400_ID = "mirage_m400_id"
+# Define the config key
+CONF_UART_ID = "uart_id"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MirageM400Component),
-        cv.GenerateID(uart.CONF_UART_ID): cv.use_id(uart.UARTComponent),
+        cv.GenerateID(CONF_UART_ID): cv.use_id(uart.UARTComponent),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
-
 async def to_code(config):
-    uart_component = await cg.get_variable(config[uart.CONF_UART_ID])
-    var = cg.new_Pvariable(config[CONF_ID], uart_component)
+    cg.add_build_flag("-std=c++17")
+    uart_component = await cg.get_variable(config[CONF_UART_ID])
+    var = cg.new_Pvt_var(config[CONF_ID], MirageM400Component)
+    cg.add(var.set_uart_parent(uart_component))
     await cg.register_component(var, config)
