ea8c54ae7db3380ad440598a177db8997e569174
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-15 01:36:40 -0600

    Update text_sensor.py

--- components/mirage_m400/text_sensor.py
+++ components/mirage_m400/text_sensor.py
@@ -1,26 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
-from esphome.const import CONF_ID, CONF_NAME
+from esphome.const import CONF_ID
 
-from . import mirage_m400_ns, MirageM400Component
+from . import mirage_m400_ns, MirageM400Component, CONF_MIRAGE_M400_ID
 
 DEPENDENCIES = ["mirage_m400"]
 
-MirageTextSensor = mirage_m400_ns.class_(
-    "MirageTextSensor", text_sensor.TextSensor, cg.Component
-)
+MirageM400TextSensor = mirage_m400_ns.class_("MirageM400TextSensor", text_sensor.TextSensor)
 
-CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend(
+CONF_MIRAGE_M400_ID = "mirage_m400_id"
+
+CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(MirageTextSensor),
-        cv.GenerateID("mirage_m400_id"): cv.use_id(MirageM400Component),
+        cv.GenerateID(): cv.declare_id(MirageM400TextSensor),
+        cv.GenerateID(CONF_MIRAGE_M400_ID): cv.use_id(MirageM400Component),
     }
-)
-
+).extend(text_sensor.SCHEMA)
 
 async def to_code(config):
-    parent = await cg.get_variable(config["mirage_m400_id"])
-    var = cg.new_Pvariable(config[CONF_ID], parent)
-    await text_sensor.register_text_sensor(var, config)
+    parent = await cg.get_variable(config[CONF_MIRAGE_M400_ID])
+    var = cg.new_Pvt_var(config[CONF_ID], MirageM400TextSensor)
     await cg.register_component(var, config)
+    cg.add(parent.register_text_sensor(var))
