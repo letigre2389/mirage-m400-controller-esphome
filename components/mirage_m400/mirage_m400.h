4c3e2bdb3bc1fde90acb30c4a6e50e693156c8c5
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-15 09:39:53 -0600

    Update mirage_m400.h

--- components/mirage_m400/mirage_m400.h
+++ components/mirage_m400/mirage_m400.h
@@ -9,46 +9,6 @@
 namespace esphome {
 namespace mirage_m400 {
 
-class MirageM400Component : public Component, public uart::UARTDevice {
- public:
-  MirageM400Component() = default;
-
-  void setup() override;
-  void loop() override;
-  void send_command(const std::string &command);
-  void register_number(class MirageM400Number *number_entity, uint8_t zone);
-  std::string last_response;
-
- private:
-  std::queue<std::string> response_buffer_;
-  std::string current_response_;
-  std::vector<class MirageM400Number *> numbers_;
-};
-
-class MirageM400Number : public number::Number, public Component {
- public:
-  void set_parent(MirageM400Component *parent) { parent_ = parent; }
-  void set_zone(uint8_t zone) { zone_ = zone; }
-
- protected:
-  void control(float value) override;
-  MirageM400Component *parent_;
-  uint8_t zone_;
-};
-
-}  // namespace mirage_m400
-}  // namespace esphome
-#pragma once
-
-#include "esphome/core/component.h"
-#include "esphome/components/uart/uart.h"
-#include "esphome/components/number/number.h"
-#include <string>
-#include <queue>
-
-namespace esphome {
-namespace mirage_m400 {
-
 // Forward declaration
 class MirageM400Number;
 
@@ -68,6 +28,7 @@ class MirageM400Component : public Component, public uart::UARTDevice {
   std::vector<MirageM400Number *> numbers_;
 };
 
+// Full definition after MirageM400Component
 class MirageM400Number : public number::Number, public Component {
  public:
   void set_parent(MirageM400Component *parent) { parent_ = parent; }
