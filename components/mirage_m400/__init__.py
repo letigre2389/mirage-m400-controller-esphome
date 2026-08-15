76a7339ee8cf9acdc0717b2f842ea77b3fd828d4
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-15 01:30:48 -0600

    Update __init__.py

--- components/mirage_m400/__init__.py
+++ components/mirage_m400/__init__.py
@@ -14,12 +14,12 @@ CONF_MIRAGE_M400_ID = "mirage_m400_id"
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MirageM400Component),
-        cv.GenerateID(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+        cv.GenerateID(uart.CONF_UART_ID): cv.use_id(uart.UARTComponent),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
-    uart_component = await cg.get_variable(config[CONF_UART_ID])
+    uart_component = await cg.get_variable(config[uart.CONF_UART_ID])
     var = cg.new_Pvariable(config[CONF_ID], uart_component)
     await cg.register_component(var, config)
