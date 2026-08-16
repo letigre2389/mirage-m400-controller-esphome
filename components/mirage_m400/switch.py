3adf3956bb9dd9c090cc037987770918b9d8cc3a
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-16 15:44:49 -0600

    Update switch.py

--- components/mirage_m400/switch.py
+++ components/mirage_m400/switch.py
@@ -3,33 +3,22 @@ import esphome.config_validation as cv
 from esphome.components import switch
 from esphome.const import CONF_ID
 
-from . import mirage_m400_ns, MirageM400Component, MirageM400Switch, CONF_MIRAGE_M400_ID
+mirage_m400_ns = cg.esphome_ns.namespace("mirage_m400")
+MirageM400Switch = mirage_m400_ns.class_(
+    "MirageM400Switch", switch.Switch, cg.Component
+)
 
-CONF_TYPE = "type"
-CONF_ZONE = "zone"
-
-SWITCH_SCHEMA = switch.SWITCH_SCHEMA.extend(
+SWITCH_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MirageM400Switch),
-        cv.Required(CONF_TYPE): cv.one_of("power", "mute"),
-        cv.Required(CONF_ZONE): cv.int_range(min=1, max=16),
+        cv.Required("name"): cv.string,
+        cv.Required("zone"): cv.int_range(min=1, max=16),
+        cv.Required("type"): cv.enum({"power": 0, "mute": 1}),
     }
 )
 
-async def to_code(config, parent=None):
+async def to_code(config, parent):
     var = cg.new_variable(config[CONF_ID], MirageM400Switch)
     await cg.register_component(var, config)
-    
-    if parent is None:
-        # Fallback for standalone use
-        parent = await cg.get_variable(config.get(CONF_MIRAGE_M400_ID))
-    
-    cg.add(parent.register_switch(var))
-    cg.add(var.set_parent(parent))
-    cg.add(var.set_zone(config[CONF_ZONE]))
-    
-    # Map type string to integer
-    type_value = 0 if config[CONF_TYPE] == "power" else 1
-    cg.add(var.set_type(cg.int(type_value)))
-    
     await switch.register_switch(var, config)
+    cg.add(parent.register_switch(var))
