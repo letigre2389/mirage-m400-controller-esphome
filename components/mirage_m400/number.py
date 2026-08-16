9a1d593381e5c9989f34e40d3590a6a5968b26f1
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-16 15:45:08 -0600

    Update number.py

--- components/mirage_m400/number.py
+++ components/mirage_m400/number.py
@@ -3,56 +3,24 @@ import esphome.config_validation as cv
 from esphome.components import number
 from esphome.const import CONF_ID
 
-from . import mirage_m400_ns, MirageM400Component, MirageM400Number, CONF_MIRAGE_M400_ID
+mirage_m400_ns = cg.esphome_ns.namespace("mirage_m400")
+MirageM400Number = mirage_m400_ns.class_(
+    "MirageM400Number", number.Number, cg.Component
+)
 
-CONF_ZONE = "zone"
-
-NUMBER_SCHEMA = number.NUMBER_SCHEMA.extend(
+NUMBER_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MirageM400Number),
-        cv.Required(CONF_ZONE): cv.int_range(min=1, max=16),
+        cv.Required("name"): cv.string,
+        cv.Required("zone"): cv.int_range(min=1, max=16),
+        cv.Optional("min_value", default=0): cv.float_,
+        cv.Optional("max_value", default=100): cv.float_,
+        cv.Optional("step", default=1): cv.float_,
     }
-).extend(cv.COMPONENT_SCHEMA)
+)
 
-async def to_code(config, parent=None):
+async def to_code(config, parent):
     var = cg.new_variable(config[CONF_ID], MirageM400Number)
     await cg.register_component(var, config)
-    
-    if parent is None:
-        # Fallback for standalone use
-        parent = await cg.get_variable(config.get(CONF_MIRAGE_M400_ID))
-    
-    cg.add(parent.register_number(var))
-    cg.add(var.set_parent(parent))
-    cg.add(var.set_zone(config[CONF_ZONE]))
-    
     await number.register_number(var, config)
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import number
-from esphome.const import CONF_ID
-
-from . import mirage_m400_ns, MirageM400Component, CONF_MIRAGE_M400_ID
-
-MirageM400Number = mirage_m400_ns.class_("MirageM400Number", cg.Component)
-
-CONF_ZONE = "zone"
-
-NUMBER_SCHEMA = number.NUMBER_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(MirageM400Number),
-        cv.GenerateID(CONF_MIRAGE_M400_ID): cv.use_id(MirageM400Component),
-        cv.Required(CONF_ZONE): cv.int_range(min=1, max=16),
-    }
-).extend(cv.COMPONENT_SCHEMA)
-
-async def to_code(config):
-    var = cg.new_variable(config[CONF_ID], MirageM400Number)
-    await cg.register_component(var, config)
-    
-    parent = await cg.get_variable(config[CONF_MIRAGE_M400_ID])
     cg.add(parent.register_number(var))
-    cg.add(var.set_parent(parent))
-    cg.add(var.set_zone(config[CONF_ZONE]))
-    
-    await number.register_number(var, config)
