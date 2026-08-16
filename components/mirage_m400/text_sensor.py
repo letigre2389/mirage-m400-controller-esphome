c423861341bc25caa2573df080a184cd323187b3
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-16 15:44:16 -0600

    Update text_sensor.py

--- components/mirage_m400/text_sensor.py
+++ components/mirage_m400/text_sensor.py
@@ -3,6 +3,28 @@ import esphome.config_validation as cv
 from esphome.components import text_sensor
 from esphome.const import CONF_ID
 
+mirage_m400_ns = cg.esphome_ns.namespace("mirage_m400")
+MirageM400TextSensor = mirage_m400_ns.class_(
+    "MirageM400TextSensor", text_sensor.TextSensor, cg.Component
+)
+
+TEXT_SENSOR_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(MirageM400TextSensor),
+        cv.Required("name"): cv.string,
+    }
+)
+
+async def to_code(config, parent):
+    var = cg.new_variable(config[CONF_ID], MirageM400TextSensor)
+    await cg.register_component(var, config)
+    await text_sensor.register_text_sensor(var, config)
+    cg.add(parent.register_text_sensor(var))
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import text_sensor
+from esphome.const import CONF_ID
+
 from . import mirage_m400_ns, MirageM400Component, MirageM400TextSensor, CONF_MIRAGE_M400_ID
 
 TEXT_SENSOR_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend(
