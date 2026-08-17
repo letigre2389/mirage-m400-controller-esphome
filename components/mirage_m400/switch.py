b352cb1275dc10e31402c03b285ebfe8436c9623
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-16 18:35:40 -0600

    Update switch.py

--- components/mirage_m400/switch.py
+++ components/mirage_m400/switch.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_NAME
 
 mirage_m400_ns = cg.esphome_ns.namespace("mirage_m400")
 MirageM400Switch = mirage_m400_ns.class_(
