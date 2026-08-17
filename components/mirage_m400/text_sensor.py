3bcad3f34a9d272daf70a3d52a0783e69062f106
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-17 15:07:47 -0600

    Update text_sensor.py

--- components/mirage_m400/text_sensor.py
+++ components/mirage_m400/text_sensor.py
@@ -10,16 +10,11 @@ CONFIG_SCHEMA = cv.Schema({
 }).extend(text_sensor.CONFIG_SCHEMA)
 
 async def to_code(config):
-    cg = cg.get_variable(cv.get_id(config))
-    # FIXED: Added 'new' and parentheses
-    cg.add_literal(cg.MirageM400TextSensor())
-    # Wait, the correct way in ESPHome is usually:
-    # var = cg.add_new_sig(cg.MirageM400TextSensor())
-    # But for a basic custom component:
     hub = cg.get_variable(cv.get_id(mirage_m400.CONF_MIRAGE_M400_ID))
-    cg.register_component(cg.MirageM400TextSensor(), config)
-    # Correct implementation for a custom entity:
+
+    # Correct C++ generation: sensor = new MirageM400TextSensor();
     sensor = cg.new_Pvariable(cg.MirageM400TextSensor())
     cg.add_expression(" %s->set_parent(%s);" % (sensor, hub))
     cg.add_expression(" %s->register_text_sensor(%s);" % (hub, sensor))
+
     cg.register_text_sensor(sensor, config)
