e25c20ce722eea96eb6698de820706a2c56ed54c
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-17 15:15:12 -0600

    Update number.py

--- components/mirage_m400/number.py
+++ components/mirage_m400/number.py
@@ -1,45 +1,24 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import number
-from esphome.const import CONF_ID, CONF_NAME
-from . import mirage_m400_ns, MirageM400Component, CONF_MIRAGE_M400_ID
+from . import mirage_m400
 
-mirage_m400_ns = cg.esphome_ns.namespace("mirage_m400")
-MirageM400Number = mirage_m400_ns.class_(
-    "MirageM400Number", number.Number, cg.Component
-)
+DEPENDENCIES = ['mirage_m400']
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(MirageM400Number),
-    cv.Required("name"): cv.string,
-    cv.Required(CONF_MIRAGE_M400_ID): cv.use_id(MirageM400Component),
-    cv.Required("zone"): cv.int_range(min=1, max=16),
-    cv.Optional("min_value", default=0): cv.float_,
-    cv.Optional("max_value", default=100): cv.float_,
-    cv.Optional("step", default=1): cv.float_,
-    cv.Optional("icon"): cv.string,
-    cv.Optional("disabled_by_default", default=False): cv.boolean,
-    cv.Optional("restore_mode", default="RESTORE_DEFAULT_OFF"): cv.string,
-    cv.Optional("mode", default="auto"): cv.enum({"auto": "auto", "slider": "slider", "box": "box"}),
-})
+    cv.Optional(number.NUMBER_ID): cv.generate_id(),
+    cv.Required(mirage_m400.CONF_ZONE): cv.int_range(1, 4),
+}).extend(number.CONFIG_SCHEMA)
 
 async def to_code(config):
-    # Create the C++ object
-    var = cg.new_variable(config[CONF_ID], MirageM400Number)
-    await cg.register_component(var, config)
+    hub = cg.get_variable(cv.get_id(mirage_m400.CONF_MIRAGE_M400_ID))
 
-    # Look up the parent hub
-    parent = await cg.get_variable(config[CONF_MIRAGE_M400_ID])
+    # Create the Number: num = new MirageM400Number();
+    num = cg.new_Pvariable(cg.MirageM400Number())
 
-    # Link the number to the hub
-    cg.add(parent.register_number(var))
-    cg.add(var.set_parent(parent))
+    cg.add_expression(" %s->set_parent(%s);" % (num, hub))
+    cg.add_expression(" %s->set_zone(%d);" % (num, config[mirage_m400.CONF_ZONE]))
+    cg.add_expression(" %s->register_number(%s);" % (hub, num))
 
-    # Register with the ESPHome core, explicitly passing the required values
-    await number.register_number(
-        var,
-        config,
-        min_value=config["min_value"],
-        max_value=config["max_value"],
-        step=config["step"]
-    )
+    cg.register_number(num, config)
+    
