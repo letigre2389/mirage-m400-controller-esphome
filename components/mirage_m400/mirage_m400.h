d577b2e89f7a72b9939169377a7b4339751941e1
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-17 14:36:43 -0600

    Update mirage_m400.h

--- components/mirage_m400/mirage_m400.h
+++ components/mirage_m400/mirage_m400.h
@@ -15,19 +15,17 @@ class MirageM400Number : public number::Number {
  public:
   void set_parent(MirageM400Component *parent) { this->parent_ = parent; }
   void control(float value);
-  void dump_config() override { ESP_LOGD("custom", "Mirage M400 Number"); }
+  void dump_config() { ESP_LOGD("custom", "Mirage M400 Number"); }
  protected:
   MirageM400Component *parent_{nullptr};
   int zone_{0};
 };
 
-// To solve the 'switch' keyword issue, we inherit from the base switch class
-// explicitly using the namespace.
-class MirageM400Switch : public esphome::components::switch::Switch {
+class MirageM400Switch : public switch::Switch {
  public:
   void set_parent(MirageM400Component *parent) { this->parent_ = parent; }
   void write_state(bool state) override;
-  void dump_config() override { ESP_LOGD("custom", "Mirage M400 Switch"); }
+  void dump_config() { ESP_LOGD("custom", "Mirage M400 Switch"); }
  protected:
   MirageM400Component *parent_{nullptr};
   int zone_{0};
@@ -38,7 +36,7 @@ class MirageM400TextSensor : public text_sensor::TextSensor {
  public:
   void set_parent(MirageM400Component *parent) { this->parent_ = parent; }
   void set_last_response(const std::string &response);
-  void dump_config() override { ESP_LOGD("custom", "Mirage M400 Text Sensor"); }
+  void dump_config() { ESP_LOGD("custom", "Mirage M400 Text Sensor"); }
  protected:
   MirageM400Component *parent_{nullptr};
 };
