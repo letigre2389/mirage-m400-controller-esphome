c28ef9e5c6e1e0f66151fb7f54ea2fd1a4dbea8c
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-17 08:14:28 -0600

    updated files

--- components/mirage_m400/mirage_m400.cpp
+++ components/mirage_m400/mirage_m400.cpp
@@ -1,96 +1,71 @@
 #include "mirage_m400.h"
-#include "esphome/core/log.h"
 
 namespace esphome {
 namespace mirage_m400 {
 
-static const char *const TAG = "mirage_m400";
-
 void MirageM400Component::setup() {
-  ESP_LOGI(TAG, "Setting up Mirage M-400 component");
+  ESP_LOGD("custom", "Mirage M400 setup");
 }
 
 void MirageM400Component::loop() {
-  if (!this->available()) {
-    return;
-  }
-
-  std::string response;
   while (this->available()) {
-    uint8_t byte = this->read();
-    if (byte == '\r' || byte == '\n') {
-      if (!response.empty() && text_sensor_) {
-        text_sensor_->set_last_response(response);
-      }
-      response.clear();
-    } else {
-      response += static_cast<char>(byte);
+    std::string response;
+    while (this->available()) {
+      char c = this->read();
+      if (c == '\n' || c == '\r') break;
+      response += c;
     }
+    if (!response.empty()) {
+      this->process_response_(response);
+    }
+  }
+}
+
+void MirageM400Component::process_response_(const std::string &response) {
+  if (this->text_sensor_) {
+    this->text_sensor_->set_last_response(response);
   }
 }
 
 void MirageM400Component::send_command(const std::string &command) {
-  this->write_str(command);
-  this->write_byte('\r');
-  delay(50);
+  this->write_str(command.c_str());
+  this->write_byte('\n');
 }
 
-void MirageM400Component::register_number(MirageM400Number *number) {
-  numbers_.push_back(number);
+void MirageM400Component::register_switch(MirageM400Switch *sw) {
+  this->switches_.push_back(sw);
+  sw->set_parent(this);
 }
 
-void MirageM400Component::register_switch(MirageM400Switch *switch_) {
-  switches_.push_back(switch_);
+void MirageM400Component::register_number(MirageM400Number *num) {
+  this->numbers_.push_back(num);
+  num->set_parent(this);
 }
 
-void MirageM400Component::register_text_sensor(MirageM400TextSensor *text_sensor) {
-  text_sensor_ = text_sensor;
+void MirageM400Component::register_text_sensor(MirageM400TextSensor *sensor) {
+  this->text_sensor_ = sensor;
+  sensor->set_parent(this);
 }
 
-void MirageM400Component::set_uart_parent(uart::UARTComponent *parent) {
-  this->set_parent(parent);
+void MirageM400Component::dump_config() {
+  ESP_LOGD("custom", "Mirage M400 Component");
 }
 
 void MirageM400Number::control(float value) {
-  auto *parent = static_cast<MirageM400Component *>(this->get_component());
-  if (parent) {
-    std::string cmd = "0401";
-    cmd += std::to_string(zone_);
-    char hex_val[3];
-    snprintf(hex_val, sizeof(hex_val), "%02X", static_cast<uint8_t>(value));
-    cmd += hex_val;
-    parent->send_command(cmd);
-  }
-}
-
-void MirageM400Number::dump_config() {
-  LOG_NUMBER("", "Mirage M-400 Volume", this);
+  if (!this->parent_) return;
+  std::string cmd = "VOL " + std::to_string(this->zone_) + " " + std::to_string((int)value);
+  this->parent_->send_command(cmd);
 }
 
 void MirageM400Switch::write_state(bool state) {
-  auto *parent = static_cast<MirageM400Component *>(this->get_component());
-  if (parent) {
-    std::string cmd;
-    if (type_ == 0) {  // POWER
-      cmd = "01";
-      cmd += (state ? "01" : "00");
-    } else if (type_ == 1) {  // MUTE
-      cmd = "02";
-      cmd += (state ? "01" : "00");
-    }
-    char zone_str[3];
-    snprintf(zone_str, sizeof(zone_str), "%02d", zone_);
-    cmd += zone_str;
-    parent->send_command(cmd);
-  }
+  if (!this->parent_) return;
+  std::string cmd = (this->type_ == 0) ? "POWER " : "MUTE ";
+  cmd += std::to_string(this->zone_) + (state ? " ON" : " OFF");
+  this->parent_->send_command(cmd);
 }
 
-void MirageM400Switch::dump_config() {
-  LOG_SWITCH("", "Mirage M-400 Switch", this);
-}
-
-void MirageM400TextSensor::dump_config() {
-  LOG_TEXT_SENSOR("", "Mirage M-400 Response", this);
+void MirageM400TextSensor::set_last_response(const std::string &response) {
+  this->publish_state(response);
 }
 
 }  // namespace mirage_m400
--- components/mirage_m400/mirage_m400.h
+++ components/mirage_m400/mirage_m400.h
@@ -1,28 +1,42 @@
 #pragma once
 
 #include "esphome.h"
-#include "text_sensor.h"
-#include "switch.h"
-#include "number.h"
+#include "esphome/components/text_sensor.h"
+#include "esphome/components/switch.h"
+#include "esphome/components/number.h"
+#include "esphome/components/uart/uart.h"
 
 namespace esphome {
 namespace mirage_m400 {
 
+class MirageM400Component;
+
 class MirageM400Number : public number::Number {
  public:
+  void set_parent(MirageM400Component *parent) { this->parent_ = parent; }
   void control(float value);
-  void dump_config() override;
+  void dump_config() { ESP_LOGD("custom", "Mirage M400 Number"); }
+ protected:
+  MirageM400Component *parent_{nullptr};
+  int zone_{0};
 };
 
 class MirageM400Switch : public switch::Switch {
  public:
+  void set_parent(MirageM400Component *parent) { this->parent_ = parent; }
   void write_state(bool state);
-  void dump_config() override;
+  void dump_config() { ESP_LOGD("custom", "Mirage M400 Switch"); }
+ protected:
+  MirageM400Component *parent_{nullptr};
+  int zone_{0};
+  int type_{0};
 };
 
 class MirageM400TextSensor : public text_sensor::TextSensor {
  public:
-  void dump_config() override;
+  void set_parent(MirageM400Component *parent) { this->parent_ = parent; }
+  void set_last_response(const std::string &response);
+  void dump_config() { ESP_LOGD("custom", "Mirage M400 Text Sensor"); }
 };
 
 class MirageM400Component : public Component, public uart::UARTDevice {
@@ -35,12 +49,10 @@ class MirageM400Component : public Component, public uart::UARTDevice {
   void register_switch(MirageM400Switch *sw);
   void register_text_sensor(MirageM400TextSensor *sensor);
   void register_number(MirageM400Number *num);
-  void set_uart_parent(uart::UARTComponent *parent);
 
  protected:
   void process_response_(const std::string &response);
 
-  // This must match the variable name used in the .cpp file loop()
   MirageM400TextSensor *text_sensor_{nullptr};
   std::vector<MirageM400Switch *> switches_;
   std::vector<MirageM400Number *> numbers_;
