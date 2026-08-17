515b1cbe7bfddfa965cc5c92df17b33cdceb4383
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-16 20:04:08 -0600

    Update mirage_m400.h

--- components/mirage_m400/mirage_m400.h
+++ components/mirage_m400/mirage_m400.h
@@ -1,49 +1,49 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/components/uart/uart.h"
-#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome.h"
+#include "esphome/components/text_sensor.h"
+#include "esphome/components/switch.h"
+#include "esphome/components/number.h"
 
 namespace esphome {
 namespace mirage_m400 {
+
+class MirageM400Number : public number::Number {
+ public:
+  void control(float value);
+  void dump_config() override;
+};
+
+class MirageM400Switch : public switch::Switch {
+ public:
+  void write_state(bool state);
+  void dump_config() override;
+};
+
+class MirageM400TextSensor : public text_sensor::TextSensor {
+ public:
+  void dump_config() override;
+};
 
 class MirageM400Component : public Component, public uart::UARTDevice {
  public:
   void setup() override;
   void loop() override;
   void dump_config() override;
-  
-  void send_command(const char *command);
-  
-  void register_text_sensor(text_sensor::TextSensor *sensor);
-  void register_switch(void *sw);
-  void register_number(void *num);
+
+  void send_command(const std::string &command);
+  void register_switch(MirageM400Switch *sw);
+  void register_text_sensor(MirageM400TextSensor *sensor);
+  void register_number(MirageM400Number *num);
+  void set_uart_parent(uart::UARTComponent *parent);
 
  protected:
-  std::vector<text_sensor::TextSensor *> text_sensors_;
-  std::vector<void *> switches_;
-  std::vector<void *> numbers_;
-  
   void process_response_(const std::string &response);
-};
 
-class MirageM400TextSensor {
- public:
-  uint8_t zone{1};
-};
-
-class MirageM400Switch {
- public:
-  uint8_t zone{1};
-  uint8_t type{0};  // 0 = power, 1 = mute
-};
-
-class MirageM400Number {
- public:
-  uint8_t zone{1};
-  float min_value{0};
-  float max_value{100};
-  float step{1};
+  // This must match the variable name used in the .cpp file loop()
+  MirageM400TextSensor *text_sensor_{nullptr};
+  std::vector<MirageM400Switch *> switches_;
+  std::vector<MirageM400Number *> numbers_;
 };
 
 }  // namespace mirage_m400
