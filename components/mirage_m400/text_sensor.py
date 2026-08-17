7abc4cc69a6aff52c3ca88b1ae2ca0b53fc18283
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-17 15:05:07 -0600

    Update text_sensor.py

--- components/mirage_m400/text_sensor.py
+++ components/mirage_m400/text_sensor.py
@@ -1,36 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
-from esphome.const import CONF_ID, CONF_NAME
+from . import mirage_m400
 
-from . import mirage_m400_ns, MirageM400Component, CONF_MIRAGE_M400_ID
+DEPENDENCIES = ['mirage_m400']
 
-# Define the C++ class for the sensor
-MirageM400TextSensor = mirage_m400_ns.class_(
-    "MirageM400TextSensor", text_sensor.TextSensor, cg.Component
-)
-
-# The ONLY schema allowed for a platform component
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(MirageM400TextSensor),
-    cv.Required(CONF_NAME): cv.string,
-    cv.Required(CONF_MIRAGE_M400_ID): cv.use_id(MirageM400Component),
-    cv.Optional("icon"): cv.string,
-    cv.Optional("disabled_by_default", default=False): cv.boolean,
-})
+    cv.Optional(text_sensor.TEXT_SENSOR_ID): cv.generate_id(),
+}).extend(text_sensor.CONFIG_SCHEMA)
 
 async def to_code(config):
-    # Create the C++ object
-    var = cg.new_variable(config[CONF_ID], MirageM400TextSensor)
-    await cg.register_component(var, config)
-
-    # Look up the parent hub using the ID from the YAML
-    # This replaces the 'parent' argument entirely
-    parent = await cg.get_variable(config[CONF_MIRAGE_M400_ID])
-
-    # Link the sensor to the hub and the hub to the sensor
-    cg.add(parent.register_text_sensor(var))
-    cg.add(var.set_parent(parent))
-
-    # Register it as a standard ESPHome text sensor
-    await text_sensor.register_text_sensor(var, config)
+    cg = cg.get_variable(cv.get_id(config))
+    # FIXED: Added 'new' and parentheses
+    cg.add_literal(cg.MirageM400TextSensor())
+    # Wait, the correct way in ESPHome is usually:
+    # var = cg.add_new_sig(cg.MirageM400TextSensor())
+    # But for a basic custom component:
+    hub = cg.get_variable(cv.get_id(mirage_m400.CONF_MIRAGE_M400_ID))
+    cg.register_component(cg.MirageM400TextSensor(), config)
+    # Correct implementation for a custom entity:
+    sensor = cg.new_Pvariable(cg.MirageM400TextSensor())
+    cg.add_expression(" %s->set_parent(%s);" % (sensor, hub))
+    cg.add_expression(" %s->register_text_sensor(%s);" % (hub, sensor))
+    cg.register_text_sensor(sensor, config)
