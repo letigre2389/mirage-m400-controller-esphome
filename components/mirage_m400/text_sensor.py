a910ee1ae305d6c5fc6647d6c8ef222f51869034
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-16 19:09:21 -0600

    Update text_sensor.py

--- components/mirage_m400/text_sensor.py
+++ components/mirage_m400/text_sensor.py
@@ -17,9 +17,6 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_MIRAGE_M400_ID): cv.use_id(MirageM400Component),
     cv.Optional("icon"): cv.string,
     cv.Optional("disabled_by_default", default=False): cv.boolean,
-    cv.Optional("restore_mode", default=cv.UseId(CONF_ID)): cv.enum(
-        "ALWAYS_OFF", "ALWAYS_ON", "RESTORE_DEFAULT_OFF", "RESTORE_DEFAULT_ON"
-    ),
 })
 
 async def to_code(config):
