bc0266e7ccfe0c900a8c69386495f81d3cc7d6e2
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-16 19:03:33 -0600

    Update switch.py

--- components/mirage_m400/switch.py
+++ components/mirage_m400/switch.py
@@ -9,17 +9,18 @@ MirageM400Switch = mirage_m400_ns.class_(
     "MirageM400Switch", switch.Switch, cg.Component
 )
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(MirageM400Switch),
-        cv.Required("name"): cv.string,
-        cv.Required(CONF_MIRAGE_M400_ID): cv.use_id(MirageM400Component),
-        cv.Required("zone"): cv.int_range(min=1, max=16),
-        cv.Required("type"): cv.enum({"power": 0, "mute": 1}),
-        cv.Optional("icon"): cv.string,
-        cv.Optional("disabled_by_default", default=False): cv.boolean,
-    }
-)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(MirageM400Switch),
+    cv.Required("name"): cv.string,
+    cv.Required(CONF_MIRAGE_M400_ID): cv.use_id(MirageM400Component),
+    cv.Required("zone"): cv.int_range(min=1, max=16),
+    cv.Required("type"): cv.enum({"power": 0, "mute": 1}),
+    cv.Optional("icon"): cv.string,
+    cv.Optional("disabled_by_default", default=False): cv.boolean,
+    cv.Optional("restore_mode", default=cv.UseID(CONF_ID)): cv.enum{
+        "ALWAYS_OFF", "ALWAYS_ON", "RESTORE_DEFAULT_OFF", "RESTORE_DEFAULT_ON"
+    ),
+})
 
 async def to_code(config):
     var = cg.new_variable(config[CONF_ID], MirageM400Switch)
