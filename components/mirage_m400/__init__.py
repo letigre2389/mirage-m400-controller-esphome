1e3bb4a04f8583cfa52f06d7f1ec467b367c2ef0
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-17 21:04:48 -0600

    Update __init__.py

--- components/mirage_m400/__init__.py
+++ components/mirage_m400/__init__.py
@@ -10,12 +10,11 @@ mirage_m400_ns = cg.esphome_ns.namespace("mirage_m400")
 MirageM400Component = mirage_m400_ns.class_("MirageM400Component", cg.Component, uart.UARTDevice)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.Required(CONF_MIRAGE_M400_ID): cv.declare_id(MirageM400Component),
     cv.Optional(CONF_UART_ID): cv.use_id(uart.UARTComponent),
 }).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
     uart_component = await cg.get_variable(config.get(CONF_UART_ID, "uart_bus"))
-    hub = cg.new_Pvariable(config[CONF_MIRAGE_M400_ID], uart_component)
+    hub = cg.new_Pvariable(config[cv.CONF_ID], uart_component)
     await cg.register_component(hub, config)
