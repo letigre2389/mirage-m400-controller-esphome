2c9ee35719b02f512bf3cb82b70b0fdca42f982c
letigre2389 <103224253+letigre2389@users.noreply.github.com>
2026-08-17 21:25:37 -0600

    Update number.py

--- components/mirage_m400/number.py
+++ components/mirage_m400/number.py
@@ -1,29 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import number
-from . import mirage_m400_ns, MirageM400Component
 from .constants import *
 
 DEPENDENCIES = ["mirage_m400"]
 
-MirageM400Number = mirage_m400_ns.class_("MirageM400Number", number.Number)
+mirage_m400_ns = cg.esphome_ns.namespace("mirage_m400")
+MirageM400Number = mirage_m400_ns.class_(
+    "MirageM400Number", number.Number, cg.Component
+)
 
 CONFIG_SCHEMA = number.number_schema(MirageM400Number).extend({
-    cv.Required(CONF_MIRAGE_M400_ID): cv.use_id(MirageM400Component),
-    cv.Required(CONF_ZONE): cv.int_range(1, 17),
-    cv.Optional("min_value", default=0): cv.float_range(0, 100),
-    cv.Optional("max_value", default=100): cv.float_range(0, 100),
-    cv.Optional("step", default=1): cv.float_,
-    cv.GenerateID(): cv.declare_id(MirageM400Number),
-    cv.Required(cv.CONF_NAME): cv.string,
+    cv.Required(CONF_MIRAGE_M400_ID): cv.use_id(mirage_m400_ns.class_("MirageM400Component")),
+    cv.Required(CONF_ZONE): cv.int_range(min=1, max=17),
 })
 
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_MIRAGE_M400_ID])
-    var = cg.new_Pvariable(
-        config[cv.GenerateID()],
-        config[cv.CONF_NAME],
-        cg.RawExpression(f"(uint8_t){config[CONF_ZONE]}"),
+    var = cg.new_Pvariable(config[cv.CONF_ID], hub)
+    await number.register_number(
+        var,
+        config,
+        min_value=0,
+        max_value=100,
+        step=1
     )
-    await number.register_number(var, config)
